660cb6509b5a00ef619062c3e639ea6ed0dca02d
Tushar Pal <tusharpal23082004@gmail.com>
2026-04-30 21:23:53 +0530

    update cors

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,16 @@ from services.market_data import init_prices
 from routers import auth, market, orders, portfolio, learn, websocket
 
 app = FastAPI(title="TradeVission API")
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://tradevission-project-production.up.railway.app",
+]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
